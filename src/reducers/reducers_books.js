f4a0cee004d6d5d098fc528d279afc039bc82a0b
Willliam Navarrete <wa.navarrete@gmail.com>
2017-10-28 06:14:47 -0700

    first commit

---
+++ src/reducers/reducers_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title: 'JavaScript: the good parts'},
+    {title: 'Hary Potter'},
+    {title: 'The Dark Tower'},
+    {title: 'Eloquent Ruby'}
+  ]
+}
